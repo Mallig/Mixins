dd6145f685069e95004d10b2927e0bc9567ba1a6
Malachy Gilchrist <malachygilchrist@gmail.com>
2019-02-26 14:49:54 +0000

    add function to draw whiskey tumbler

--- src/drink.js
+++ src/drink.js
@@ -1,22 +1,22 @@
 export default class Drink {
-    constructor(glass, ingredients) {
-        this.glass = glass;
+    constructor(glassType, ingredients) {
+        this.glassType = glassType;
         this.ingredients = ingredients;
     }
 
     glassDraw(x = 55, y = 80) {
-        if (this.glass === "tumbler") {
-            return [[x, y, x, y+40], [x, y+40, x+40, y+40], [x+40, y+40, x+40, y]]
-        } else if (this.glass === "cocktail") {
+        if (this.glassType === "tumbler") {
+            return this.drawTumbler()
+        } else if (this.glassType === "cocktail") {
             return this.drawCocktailGlass()
         }
     }
 
     drawCocktailGlass(canvasHeight = 150, canvasWidth = 150) {
-        let stemHeight = 50
-        let baseWidth = 40
-        let bowlWidth = 50
-        let bowlHeight = 25
+        let stemHeight = 45
+        let baseWidth = 50
+        let bowlWidth = 60
+        let bowlHeight = 35
 
         let x = canvasWidth/2 - baseWidth/2
         let y = canvasHeight/2 + (stemHeight + bowlHeight)/2
@@ -27,6 +27,20 @@ export default class Drink {
         let rightRim = [x + baseWidth/2, y - stemHeight, x + baseWidth/2 + bowlWidth/2, y - stemHeight - bowlHeight]
     
         return [base, stem, leftRim, rightRim]
+    }
+
+    drawTumbler(canvasWidth = 150, canvasHeight = 150) {
+        let baseWidth = 60
+        let bowlHeight = 60
+
+        let x = canvasWidth/2 - baseWidth/2
+        let y = canvasHeight/2 + bowlHeight/2
+
+        let base = [x, y, x + baseWidth, y]
+        let leftRim = [x, y, x, y - bowlHeight]
+        let rightRim = [x + baseWidth, y, x + baseWidth, y - bowlHeight]
+
+        return [base, leftRim, rightRim]
     }
 
 }
